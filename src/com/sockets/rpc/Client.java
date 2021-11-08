aa74c432be33961957522da7859ea9edb3dc3b34
nandan-pai <53964874+nandan-pai@users.noreply.github.com>
2021-11-08 18:57:29 +0530

    minor changes made

--- src/com/sockets/rpc/Client.java
+++ src/com/sockets/rpc/Client.java
@@ -8,7 +8,9 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-	    String ipAddress = "192.168.1.2";
+	    //Enter ip address of server here 
+	    String ipAddress = "";
+	    //Enter port number on which client will be listening here
 	    int port = 5000;
 	    Socket soc = new Socket(ipAddress, port);
 
