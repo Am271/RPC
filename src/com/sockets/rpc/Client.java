89fc59636eb19194d2c93b1c7de9158137256ee8
Amitash <amitu895@gmail.com>
2021-11-14 21:22:39 +0530

    Added JSON support to multithreaded server

--- src/com/sockets/rpc/Client.java
+++ src/com/sockets/rpc/Client.java
@@ -5,20 +5,25 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import org.json.simple.JSONObject;
 
 public class Client {
     public static void main(String[] args) throws Exception {
 	    //Enter ip address of server here 
-	    String ipAddress = "";
+	    String ipAddress = "localhost";
 	    //Enter port number on which client will be listening here
-	    int port = 5000;
+	    int port = 1234;
 	    Socket soc = new Socket(ipAddress, port);
 
-	    String request = "Hello Server";
+	    String message = "Hello Server";
+        JSONObject obj = new JSONObject();
+        obj.put("message", message);
 
         OutputStreamWriter outObj = new OutputStreamWriter(soc.getOutputStream());
+        // outObj.write(obj.toString());
         PrintWriter out = new PrintWriter(outObj);
-        out.println(request);
+        // out.println(obj.toString());
+        out.println("BROADCAST");
         outObj.flush();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
