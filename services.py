35f4564464361cad76ec045897c15b2ac4dac1ae
Amitash <amitu895@gmail.com>
2021-11-21 11:46:56 +0530

    Added documentation and a simple representation of a method call from the stub

---
+++ services.py
@@ -0,0 +1,6 @@
+class service:
+	#This class provides all the functions that the RPC server will present to the client for calling
+	def add(self, x, y):
+		'''(number, number) -> number\nReturns a sum of two numbers'''
+		return x + y
+	#The above function is a sample for other functions to be defined later
--- stub.py
+++ stub.py
@@ -1,10 +1,17 @@
-class service:
-	def add(x, y):
-		'''(number, number) -> number\nReturns a sum of two numbers'''
-		return x + y
+import services
 
-obj = service()
+# Create an object of class services
+obj = services.service()
 
+# Collects all the methods bound to the object 'obj', excluding dunder methods
 method_list = [func for func in dir(obj) if callable(getattr(obj, func)) and not func.startswith("__")]
 print(method_list)
-print(obj.add.__doc__)
+
+for i in method_list:
+	# getattr(object, function_name_as_str) returns the address of the method to be able to call it
+	# It returns a reference to the method in the object
+	print(getattr(obj, i)(10, 2))
+	# The above statement obtains the reference to all the methods in the methodlist
+	# and calls them passing 10 & 2 as parameters
+
+# print(obj.add.__doc__)
