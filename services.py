74a7efd69db6a40db6426e472076298b251658be
Amitash <amitu895@gmail.com>
2021-11-25 22:04:28 +0530

    Made minor changes, fixed indentation

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 class service:
 	#This class provides all the functions that the RPC server will present to the client for calling
-	def add(self, x, y):
-		'''(number, number) -> number --Returns a sum of two numbers'''
-		return x + y
+    def add(self, x, y):
+        '''(number, number) -> number --Returns a sum of two numbers'''
+        x = float(x); y = float(y)
+        return str(x + y)
 	#The above function is a sample for other functions to be defined later
--- stub.py
+++ stub.py
@@ -9,54 +9,54 @@ method_list = []
 # Funtion to return a JSON formatted string containing all available services and their description
 def broadcast():
 	# Create an object of class services
-	obj = services.service()
+    obj = services.service()
 
 	# Populate the list of methods available to call
-	method_list = [func for func in dir(obj) if callable(getattr(obj, func)) and not func.startswith("__")]
+    method_list = [func for func in dir(obj) if callable(getattr(obj, func)) and not func.startswith("__")]
 	# getattr(object, function_name_as_str) returns the address of the method to be able to call it
 
 	# Prepare a JSON string to send to client
-	json_string = "{"
+    json_string = "{"
 
-	for i in method_list:
+    for i in method_list:
 
 		# Add method names and method descriptions to JSON
-		json_string += '\n"' + i + '" : "' + getattr(obj, i).__doc__ + '",'
+        json_string += '\n"' + i + '" : "' + getattr(obj, i).__doc__ + '",'
 
 	# Format the JSON string by removing the last ',' and adding the closing '}'
-	json_string = json_string[:-1] + "\n}"
+    json_string = json_string[:-1] + "\n}"
 
 	# Return the json formatted string to server
-	return json_string
+    return json_string
 
 
 # Function to process requests other than broadcasts from the server
 def process(req):
-	call_dict = dict()
-	try:
-		call_dict = eval(req)
-	except:
-		return "Malformed JSON received, send request again"
-    
-    if not isinstance(req.get('call'), int): # Checks if 'call' is a key in the request JSON, also checks if the value is an integer
+    call_dict = dict()
+    try:
+        call_dict = eval(req)
+    except:
         return "Malformed JSON received, send request again"
-    
-    if not len(req.keys()) - 1 == req.get('call'): # Checks if the number of functions declared equals the number of keys - 1 in the JSON
+
+    if not isinstance(call_dict.get('call'), int): # Checks if 'call' is a key in the request JSON, also checks if the value is an integer
+        return "oooMalformed JSON received, send request again"
+
+    if not len(call_dict.keys()) - 1 == call_dict.get('call'): # Checks if the number of functions declared equals the number of keys - 1 in the JSON
         return "Number of functions declared is not equal to the number of function calls in the JSON"
-    
-	else:
-		# Create an object to perform function calls
-		obj = services.service()
-		res_json = { 'results' : 0, 'errors' : [] } # Result in JSON to be returned
-		success = 0 # Keeps count of number of functions that were successfully executed
-		# Perform function calls
-		for k, v in call_dict:
-			if k == 'call': # If the key is 'call', the loop continues to the next key
-				continue
-			try:
-				res = getattr(obj, k)(*v) # Attempts to obtain reference to function and perform the function call
-				res_json[k] = res # Stores the result in JSON
-				res_json['results'] += 1 # Counts the number of successful results
-			except TypeError:
-				res_json['errors'].append(k) # Stores the function names with incorrect list of parameters
-        return res_json
+
+    else:
+        # Create an object to perform function calls
+        obj = services.service()
+        res_json = { 'results' : 0, 'errors' : [] } # Result in JSON to be returned
+        success = 0 # Keeps count of number of functions that were successfully executed
+        # Perform function calls
+        for k, v in call_dict.items():
+            if k == 'call': # If the key is 'call', the loop continues to the next key
+                continue
+            try:
+                res = getattr(obj, k)(*v) # Attempts to obtain reference to function and perform the function call
+                res_json[k] = res # Stores the result in JSON
+                res_json['results'] += 1 # Counts the number of successful results
+            except TypeError:
+                res_json['errors'].append(k) # Stores the function names with incorrect list of parameters
+    return str(res_json)
